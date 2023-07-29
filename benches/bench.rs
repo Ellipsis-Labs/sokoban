ec3c188da96f45952487f6bebe3e7817f9aa7f59
febo <febo@metaplex.com>
2023-07-29 21:37:34 +0100

    Add bench tests

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,6 +6,7 @@ extern crate test;
 mod bench_tests {
     use rand::seq::SliceRandom;
     use rand::{self, Rng};
+    use sokoban::hash_set::HashSet;
     use sokoban::node_allocator::FromSlice;
     use sokoban::node_allocator::NodeAllocatorMap;
     use sokoban::*;
@@ -21,6 +22,7 @@ mod bench_tests {
     type SHashMap = HashTable<u128, u128, NUM_BUCKETS, MAX_SIZE>;
     type AVLTreeMap = AVLTree<u128, u128, MAX_SIZE>;
     type CritbitTree = Critbit<u128, NUM_NODES, MAX_SIZE>;
+    type SHashSet = HashSet<u128, MAX_SIZE>;
 
     const NUM_BUCKETS_1K: usize = 1000;
     const NUM_NODES_1K: usize = (1001 << 1) + 1;
@@ -29,6 +31,7 @@ mod bench_tests {
     type SHashMap1K = HashTable<u128, u128, NUM_BUCKETS_1K, 2001>;
     type AVLTreeMap1K = AVLTree<u128, u128, 1001>;
     type CritbitTree1K = Critbit<u128, NUM_NODES_1K, 1001>;
+    type SHashSet1k = HashSet<u128, 1001>;
 
     #[bench]
     fn bench_std_btree_map_insert_1000_u128(b: &mut Bencher) {
@@ -101,6 +104,18 @@ mod bench_tests {
     }
 
     #[bench]
+    fn bench_sokoban_hash_set_insert_1000_u128(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let mut buf = vec![0u8; std::mem::size_of::<SHashSet1k>()];
+        let s = SHashSet1k::new_from_slice(buf.as_mut_slice());
+        b.iter(|| {
+            for _ in 0..1000 {
+                s.insert(rng.gen::<u128>());
+            }
+        })
+    }
+
+    #[bench]
     fn bench_sokoban_red_black_tree_insert_1000_u128_stack(b: &mut Bencher) {
         let mut rng = rand::thread_rng();
         let mut m = RBTree1K::new();
@@ -140,6 +155,17 @@ mod bench_tests {
         b.iter(|| {
             for v in 0..1000 {
                 m.insert(v as u128, rng.gen::<u128>());
+            }
+        })
+    }
+
+    #[bench]
+    fn bench_sokoban_hash_set_insert_1000_u128_stack(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let mut s = SHashSet1k::new();
+        b.iter(|| {
+            for _ in 0..1000 {
+                s.insert(rng.gen::<u128>());
             }
         })
     }
@@ -210,6 +236,18 @@ mod bench_tests {
         b.iter(|| {
             for v in 0..20000 {
                 m.insert(v as u128, rng.gen::<u128>());
+            }
+        })
+    }
+
+    #[bench]
+    fn bench_sokoban_hash_set_insert_20000_u128(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let mut buf = vec![0u8; std::mem::size_of::<SHashSet>()];
+        let s = SHashSet::new_from_slice(buf.as_mut_slice());
+        b.iter(|| {
+            for _ in 0..20000 {
+                s.insert(rng.gen::<u128>());
             }
         })
     }
@@ -314,6 +352,22 @@ mod bench_tests {
         })
     }
 
+    #[bench]
+    fn bench_sokoban_hash_set_remove_1000_u128(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let mut buf = vec![0u8; std::mem::size_of::<SHashSet>()];
+        let s = SHashSet::new_from_slice(buf.as_mut_slice());
+        let mut slice: Vec<u128> = (0..1000).collect();
+        slice.shuffle(&mut rng);
+        for _ in 0..1000 {
+            s.insert(rng.gen::<u128>());
+        }
+        b.iter(|| {
+            for k in slice.iter() {
+                s.remove(k);
+            }
+        })
+    }
 
     #[bench]
     fn bench_std_btree_map_lookup_20000_u128(b: &mut Bencher) {
@@ -399,6 +453,20 @@ mod bench_tests {
         b.iter(|| {
             for v in 0..20000 {
                 m.get(&v);
+            }
+        })
+    }
+
+    #[bench]
+    fn bench_sokoban_hash_set_lookup_20000_u128(b: &mut Bencher) {
+        let mut buf = vec![0u8; std::mem::size_of::<SHashSet>()];
+        let s = SHashSet::new_from_slice(buf.as_mut_slice());
+        for v in 0..20000 {
+            s.insert(v);
+        }
+        b.iter(|| {
+            for v in 0..20000 {
+                s.contains(&v);
             }
         })
     }
